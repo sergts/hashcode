670263a11baa43c973b84ccb70b51d6598f7f500
Eduard Netšajev <themediaisajoke@gmail.com>
2016-02-11 20:25:43 +0200

    added Product and ProductType

--- src/model/Product.java
+++ src/model/Product.java
@@ -1,4 +1,15 @@
 package model;
 
 public class Product {
+
+    ProductType productType;
+
+    int amount;
+
+    public Product() {}
+
+    public Product(ProductType productType, int amount) {
+        this.productType = productType;
+        this.amount = amount;
+    }
 }
--- src/model/ProductType.java
+++ src/model/ProductType.java
@@ -1,4 +1,34 @@
 package model;
 
 public class ProductType {
+
+    int id;
+
+    int weight;
+
+    public ProductType() {}
+
+    public ProductType(int id, int weight) {
+        this.id = id;
+        this.weight = weight;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ProductType that = (ProductType) o;
+
+        if (id != that.id) return false;
+        return weight == that.weight;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + weight;
+        return result;
+    }
 }
